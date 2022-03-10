058e8f6c01f6aedda5448ae1198e771de0cc2432
Brayden <Bsparks.dev-10.com>
2022-03-10 10:24:06 -0600

    now takes args from the user

--- ConditionalLoopAndMethod/Program.cs
+++ ConditionalLoopAndMethod/Program.cs
@@ -6,14 +6,22 @@ namespace ConditionalLoopAndMethod
     {
         static void Main(string[] args)
         {
-            // TODO: Implement method to check if a number is in range
+            Console.Write("Enter an initial number: ");
+            int num = Int32.Parse(Console.ReadLine());
 
+            Console.Write("Enter a target number: ");
+            int target = Int32.Parse(Console.ReadLine());
 
+            Console.Write("Enter a range: ");
+            int range = Int32.Parse(Console.ReadLine());
+
+            // TODO: Write the result by calling IsWithinRange
+            Console.WriteLine($"Given a starting number of: {num} and a target number: {target}. It's {IsWithinRange(num,target,range)} ");
         }
 
-        private static bool IsWithinRange(int num1, int target, int range)
+        private static bool IsWithinRange(int num, int target, int range)
         {
-            return (num1 - target) <= range;
+            return (num - target) <= range;
         }
 
     }
